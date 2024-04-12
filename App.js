0c4e9a41ac8ff75f7e2d8670a9bbb074e7c6345f
Mohit Vora <mohitvora155@gmail.com>
2024-04-12 14:26:29 +0530

    minor changes

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigationTabs from './src/navigation/navigationTabs';
 import screens from './src/constants/screens';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
